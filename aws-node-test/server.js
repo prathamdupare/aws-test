f351722923bc109797da1574c8e3a3ca59e7ec4e
prathamdupare <prathamdupare@proton.me>
2026-06-27 15:09:27 +0530

    updated policy changes

--- aws-node-test/server.js
+++ aws-node-test/server.js
@@ -5,7 +5,7 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.get('/api/health', (req, res) => {
-  res.json({ status: 'success', message: 'Hello from AWS EC2 + PM2 and Pratham!' });
+  res.json({ status: 'success', message: 'Hello from AWS EC2 + PM2 !' });
 });
 
 app.listen(PORT, () => {
--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <p>Your questions answered</p>
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
