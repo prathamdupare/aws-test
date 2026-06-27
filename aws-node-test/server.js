d4ebca241acb55b12f4b85dd587e7fb20e2b4264
prathamdupare <prathamdupare@proton.me>
2026-06-27 13:17:14 +0530

    made small changes to check actions

--- aws-node-test/server.js
+++ aws-node-test/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/api/health', (req, res) => {
-  res.json({ status: 'success', message: 'Hello from AWS EC2 + PM2!' });
+  res.json({ status: 'success', message: 'Hello from AWS EC2 + PM2 and Pratham!' });
 });
 
 app.listen(PORT, () => {
